5586cd70746525911d20b93e274aeb57cedd3c38
Tanzim Ahmed <32319601+TanzimRusho@users.noreply.github.com>
2021-07-18 19:49:29 +0600

    Create UVa_374.cpp

---
+++ UVa_374.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+long long int bigmod(long long int B, long long int P, long long int M)
+{
+    if(P==0) return 1;
+    else if (P%2) return ((B%M) * bigmod(B, P-1, M))%M;
+    else {
+        long long int d = bigmod(B, P/2, M);
+        return ((d%M)*(d%M))%M;
+    }
+}
+
+int main()
+{
+    long long int B, P, M, result;
+    
+    while(scanf("%lld\n%lld%lld\n\n", &B, &P, &M)==3)
+    {
+        result = bigmod(B, P, M);
+        cout << result << endl;
+    }
+    
+    return 0;
+}
