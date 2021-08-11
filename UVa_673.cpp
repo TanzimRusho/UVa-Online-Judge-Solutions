6bb9a85c281b2f563680f25b8ea0b5d410368277
Tanzim Ahmed <32319601+TanzimRusho@users.noreply.github.com>
2021-08-12 02:59:28 +0600

    got WA.

---
+++ UVa_673.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+static char stack[256];
+static char par[256];
+int top;
+
+int main()
+{
+    unsigned n, error;
+    
+    scanf("%u", &n);
+    //getchar();
+    
+    while(n--)
+    {
+        scanf("%s", &par);        
+        
+        if (strlen(par) & 1)
+            printf("No\n");
+            
+        else
+        {
+            stack[256] = '\0';
+            top = -1;
+            error = 0;
+            
+            for (unsigned i=0; par[i]; ++i)
+            {
+                if(par[i] == ' ')
+                    continue;
+                
+                if(par[i] == '(' || par[i] == '[')
+                    stack[++top] = par[i];
+                    
+                else if((par[i] == ')' && stack[top] == '(') || 
+                    (par[i] == ']' && stack[top] == '['))
+                    --top;
+                    
+                else 
+                {
+                    error = 1;
+                    break;
+                }
+                
+            }
+            (error || top > -1) ? printf("No\n") : printf("Yes\n");    
+        }
+    }
+    
+    return 0;
+}
